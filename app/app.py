f452d73fae43c5221c78223169a0a69fb4d01d53
Kamran Khot <kamran29khot@gmail.com>
2023-04-07 20:39:57 +0530

    initial commit

---
+++ app/app.py
@@ -0,0 +1,192 @@
+
+from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+from utils.disease import disease_dic
+from utils.fertilizer import fertilizer_dic
+import requests
+import config
+import pickle
+import io
+import torch
+from torchvision import transforms
+# from PIL import Image
+from utils.model import ResNet9
+
+
+
+
+
+
+
+
+
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+
+
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+@ app.route('/')
+def home():
+    title = 'Home'
+    return render_template('index.html', title=title)
+
+
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+
+
+
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
+
+
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+       
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+
+
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'BioHarvest - Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
+
+
+
+
+filename = 'models/random_forest_regression_model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+@ app.route('/air_prediction')
+def air_prediction():
+    title = 'Air quality prediction'
+    return render_template('air_prediction.html', title=title)
+
+@app.route('/air_prediction', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        t = int(request.form['T'])
+        tm = int(request.form['TM'])
+        tmm = int(request.form['Tm'])
+        slp = int(request.form['SLP'])
+        h = int(request.form['H'])
+        vv = float(request.form['VV'])
+        v = float(request.form['V'])
+        vm = int(request.form['VM'])
+       
+        data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+        my_prediction = classifier.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
